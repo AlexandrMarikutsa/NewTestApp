de231e3dc0063c9ce86fee20d481ab0d584892ea
AlexandrMarikutsa <a.marikutsa@ukr.net>
2015-10-12 12:02:26 +0300

    changed toggle buttons

--- app/src/main/java/com/demo/develop/newtestapp/MainActivity.java
+++ app/src/main/java/com/demo/develop/newtestapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.demo.develop.newtestapp;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -60,7 +61,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
     private void initViews(){
         graph = (Button)findViewById(R.id.graph);
         for (int id : buttonIds) {
-            ToggleButton button = (ToggleButton) findViewById(id);
+            final ToggleButton button = (ToggleButton) findViewById(id);
             button.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
                 @Override
                 public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
